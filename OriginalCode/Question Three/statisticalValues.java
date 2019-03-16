66a05fb13f8282555fa2bf4ec43d45ecbb37f22b
Joan S. Quintero <JoanSQuintero@gmail.com>
2019-03-16 19:51:01 -0400

    removed comments

--- OriginalCode/Question Three/statisticalValues.java
+++ OriginalCode/Question Three/statisticalValues.java
@@ -1,76 +1,73 @@
 package question3;
 
 import java.util.ArrayList;
-
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Scanner;
 
 import question3.statisticalValues.Global;
 
-class statisticalValues implements Runnable  // parent thread
+class statisticalValues implements Runnable  
 { 
 
 	public void run() 
 	{ 
 		try
 		{ 
-			System.out.println ("The average value is: "+ Global.average);
+		    System.out.println ("The average value is: "+ Global.average);
 		    System.out.println ("The maximum value is: "+ Global.maximum);
 		    System.out.println ("The minimum value is: "+ Global.minimum);
 		} 
 		catch (Exception e) 
-		{                     // Throwing an exception 
+		{                   
 			System.out.println ("Exception is caught"); 
 		} 
 	}
 	
-	public static class Global {
-		static ArrayList<Integer> alist=new ArrayList<Integer>();
+	public static class Global 
+	{
+	    static ArrayList<Integer> alist = new ArrayList<Integer>();
 	    public static int maximum;
 	    public static int minimum;
 	    public static int average;
 	}
  
-// Main Class 
 	public static void main(String[] args) 
 	
 	{
 		int input = 0;
-		Scanner reader = new Scanner(System.in);  // Reading from System.in
+		Scanner reader = new Scanner(System.in);  
 		System.out.println("Enter a number: ");
 		input = reader.nextInt();
 		Global.alist.add(input);
 		
-		if (input <= 0) // if input = 0, stop user inputs
+		if (input <= 0) 
 		{ 
 			Global.alist.remove(Global.alist.size() - 1);
-			System.out.println(Global.alist); // only here for testing
-			System.out.println(Global.alist.size()); // only here for testing
 			
-			Thread object = new Thread(new statisticalValues()); //parent
-			Thread maximum = new Thread(new calculateMaximum()); //child maximum
-			Thread minimum = new Thread(new calculateMinimum()); //child minimum
-			Thread average = new Thread(new calculateAverage()); //child average
-			average.start(); // start average thread
-			minimum.start(); // start minimum thread
-			maximum.start(); // start maximum thread
-			object.start();  // restart parent thread
+			Thread object = new Thread(new statisticalValues()); 
+			Thread maximum = new Thread(new calculateMaximum()); 
+			Thread minimum = new Thread(new calculateMinimum()); 
+			Thread average = new Thread(new calculateAverage()); 
+			average.start(); 
+			minimum.start(); 
+			maximum.start(); 
+			object.start();  
 		}
 		
 		else 
 		{
-			main(args); // Recalling main to ask user for more inputs
+			main(args); 
 		}
-		reader.close(); // Done listening for inputs
+		reader.close(); 
 	}
 }
 
 class calculateMaximum implements Runnable
 {
 	@Override
-	public void run() {
-		// TODO Auto-generated method stub
+	public void run() 
+	{
 		Global.maximum = Collections.max(Global.alist);   
 	}
 }
@@ -78,8 +75,8 @@ class calculateMaximum implements Runnable
 class calculateMinimum implements Runnable
 {
 	@Override
-	public void run() {
-		// TODO Auto-generated method stub
+	public void run() 
+	{
 		Global.minimum = Collections.min(Global.alist);
 	}
 }
@@ -87,10 +84,11 @@ class calculateMinimum implements Runnable
 class calculateAverage implements Runnable
 {
 	@Override
-	public void run() {
-		// TODO Auto-generated method stub
+	public void run() 
+	{
 		int count = 0;
-		for (int i: Global.alist) {
+		for (int i: Global.alist) 
+		{
 			count += i;
 		}
 		Global.average = count/Global.alist.size();

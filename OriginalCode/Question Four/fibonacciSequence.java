d5d904bde2d99c20f83394d987c0fd31de432ad8
Joan S. Quintero <JoanSQuintero@gmail.com>
2019-03-16 19:59:23 -0400

    removed comments

--- OriginalCode/Question Four/fibonacciSequence.java
+++ OriginalCode/Question Four/fibonacciSequence.java
@@ -7,70 +7,68 @@ import java.util.stream.IntStream;
 public class fibonacciSequence implements Runnable {
 
 	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-		
-		try {
+	public void run() 
+	{
+		try 
+		{
 			System.out.println ("The sequence is: ");
 			System.out.println((Global.sequence)); 
-		    
-		}catch (Exception e) {
-			
 		}
+		catch (Exception e) {}
 	}
 	
-	public static class Global {
+	public static class Global 
+	{
 		static ArrayList<Integer> sequence=new ArrayList<Integer>();
 		static int input = 0;
 	}
 	
-	
 	public static void main(String[] args) throws InterruptedException
 	{
-		
-		
 		Scanner reader = new Scanner(System.in);
 		System.out.println("How many fibonnaci numbers do you want?\n" + 
 		"Enter an amount: ");
 		int input = reader.nextInt();
 		
-		if (input <= 0) {
+		if (input <= 0) 
+		{
 			System.out.println("Sorry, not accepting inputs that are less than 1\n"+
 		"...\n"+
 		"...\n"+
 		"why don't you try again?\n");
-			main(args); // wrong input, go again
+			main(args); 
 		}
 		
-		else {
+		else 
+		{
 			Global.input = input;
-			Thread generateFib = new Thread(new generateFib()); //child average
-			generateFib.start(); // start average thread
+			Thread generateFib = new Thread(new generateFib()); 
+			generateFib.start(); 
 			Thread parent = new Thread(new fibonacciSequence());
 			parent.start();
 			parent.join();
-		
 		}
 		reader.close();
 		
 		}
 }
 
-class generateFib implements Runnable {
+class generateFib implements Runnable 
+{
 
 	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-		int f1 = 0, f2 = 1, i;    
-		fibonacciSequence.Global.sequence.add(f1);
-		fibonacciSequence.Global.sequence.add(f2);
+	public void run() 
+	{
+		int n1 = 0, n2 = 1, i;    
+		fibonacciSequence.Global.sequence.add(n1);
+		fibonacciSequence.Global.sequence.add(n2);
 	  
 	    for (i = 0; i <= fibonacciSequence.Global.input-3; i++) 
 	    { 
-	        int next = f1 + f2; 
-	        f1 = f2; 
-	        f2 = next; 
-	        fibonacciSequence.Global.sequence.add(f2);
-	        }
+	        int next = n1 + n2; 
+	        n1 = n2; 
+	        n2 = next; 
+	        fibonacciSequence.Global.sequence.add(n2);
 	    }
 	}
+}
